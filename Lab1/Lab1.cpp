c2e9a8a590c714ebe474f46752a2ff7fa7f4f9d7
Xeltalliv <vadim@vvv.kiev.ua>
2021-09-15 13:55:55 +0300

    Added 1st lab

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,31 @@
+// Lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#define PI 3.141592653589793
+
+int main()
+{
+    double height, radius, cillinderVolume, coneVolume;
+    std::cout << "Enter the height of the shape: ";
+    std::cin >> height;
+    std::cout << "\nEnter the radius of the shape: ";
+    std::cin >> radius;
+    cillinderVolume = radius * PI * height;
+    coneVolume = 1.0 / 3.0 * PI * radius * radius * height;
+    std::cout << "\nThe volume of the shape: " << cillinderVolume;
+    std::cout << "\nThe volume of the shape: " << coneVolume << "\n";
+    system("pause");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
