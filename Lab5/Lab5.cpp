b73ed2946a4a7e4e7e94ca8017fca00956c84b29
Xeltalliv <vadim@vvv.kiev.ua>
2021-11-03 00:14:23 +0200

    Add Lab5

---
+++ Lab5/Lab5.cpp
@@ -0,0 +1,34 @@
+﻿// Lab5.cpp
+
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+//Задача: Знайти цифрові корені всіх простих чисел з інтервалу [100, 200]
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+	//setvbuf(stdout, nullptr, _IOFBF, 1000);
+
+	std::cout << "Задача: Знайти цифрові корені всіх простих чисел з інтервалу [100, 200]\n";
+	std::string divider(119, '=');
+	std::cout << divider << "\n";
+
+	for (int iterator = 100; iterator < 201; iterator++) {
+		int number = iterator;
+		std::string globalString(std::to_string(number) + " --> ");
+		while (number >= 10) {
+			int newNumber = 0;
+			std::string localString("");
+			while (number >= 10) {
+				int digit = number % 10;
+				newNumber += digit;
+				localString = " + " + std::to_string(digit) + localString;
+				number = number / 10;
+			}
+			newNumber += number;
+			globalString += std::to_string(number) + localString + " --> ";
+			number = newNumber;
+			std::cout << globalString + std::to_string(number) + "\n";
+		}
+	}
+}
